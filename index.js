b58bc99b1f779aea3233a31b8b318b755f36e02f
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-09-23 21:56:39 +0100

    completed asseignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCase = tutorial => {
+  return tutorial
+    .split(' ') // Split the title into an array of words
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+    .join(' '); // Join the words back into a single string
+};
+
+const titleCaseTutorials = tutorials.map(titleCase);
+
+console.log(titleCaseTutorials);
